7885a2603caa850d910d0cecbc237f9a22e361f4
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-08 19:05:48 -0800

    added component to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+//components
+import BookList from './components/Booklist';
+
+
 class App extends Component {
   render() {
     return (
       <div id="main">
       <h1> Luke's React </h1>
+      <BookList/>
       </div>
     );
   }
