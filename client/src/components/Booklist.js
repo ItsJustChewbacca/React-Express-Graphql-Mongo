17134d4d0ff3dbbc28a0bc0544daa1fac7a2b9e8
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-09 00:48:33 -0800

    create constructor and event for selecting a book

--- client/src/components/Booklist.js
+++ client/src/components/Booklist.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 
+//components
+
+import BookDetails from './BookDetails';
+
 class BookList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
   displayBooks(){
     var data = this.props.data;
     if(data.loading) {
       return( <div>Loading books...</div> );
     } else {
-        return data.books.map(book => {
-          return(<li>{book.name}</li>)
+        return data.books.map(book => {return(<li key={ book.id } onClick={(e) => {this.setState({ selected: book.id})}}>{book.name}</li>)
         })
       }
   }
@@ -18,8 +27,9 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">
-          <li>{ this.displayBooks() }</li>
+          { this.displayBooks() }
         </ul>
+        <BookDetails bookid={this.state.selected}/>
       </div>
     );
   }
