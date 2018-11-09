090feef60d47c60771157ed8ccf8a9729cc7d5a3
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-08 17:19:44 -0800

    connected mongoose with Mlab instance

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const app = express();
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+const app = express();
+
+// Connet to Mlab instance
+mongoose.connect('mongodb://Luke:redux11@ds121192.mlab.com:21192/gql');
+
+mongoose.connection.once('open', () => {
+  console.log("Connected to Database")
+})
 
 app.use('/graphql', graphqlHTTP ({
   schema // ES6
 }));
+
 app.listen(4000, () => {
   console.log('now listening for requests on port 4000')
 })
