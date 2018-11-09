3f8b8ec94f215f36a6b7bf4e1e1d4d86fc744783
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-08 16:28:41 -0800

    loaded schema in middlewear

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const app = express();
-
+const schema = require('./schema/schema')
 
 app.use('/graphql', graphqlHTTP ({
-
+  schema // ES6
 }));
 app.listen(4000, () => {
   console.log('now listening for requests on port 4000')
